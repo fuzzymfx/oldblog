7ed2df5bc558f0559c8c19fa85c6a30dda0f983f
Anubhab Patnaik <anubhabr50@gmail.com>
2022-05-26 13:44:19 +0530

    changed meta tags and title

--- src/components/BlogIntro.jsx
+++ src/components/BlogIntro.jsx
@@ -7,7 +7,11 @@ export default function BlogIntro() {
 		<div style={{textAlign: 'center', width:'100%' }} className=" ">
 			<h1 className="mx-5" id='head' style={{color: '#6c63ff', fontFamily: 'Smooch', fontSize: '100px'}}>
 				BLOGS
+				
+
+
 			</h1>
+
 		</div>
 	)
 }
