ee99c68a18e53fb31d13b8d1f669296fc6cd23a8
Anubhab Patnaik <anubhabr50@gmail.com>
2022-05-26 09:41:46 +0530

    update css

--- src/components/BlogIntro.jsx
+++ src/components/BlogIntro.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import '../assets/css/blog.css';
 
 
 export default function BlogIntro() {
 	return (
-		<div style={{textAlign: 'center'}}>
-			<h1 id='head' style={{color: '#6c63ff', fontFamily: 'Smooch', fontSize: '150px'}}>
+		<div style={{textAlign: 'center', width:'100%' }} className=" ">
+			<h1 className="mx-5" id='head' style={{color: '#6c63ff', fontFamily: 'Smooch', fontSize: '100px'}}>
 				BLOGS
 			</h1>
 		</div>
