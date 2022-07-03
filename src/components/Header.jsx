8e80d6bbc20075ca2fe6ccc1d263133fa486a9b2
Anubhab Patnaik <anubhabr50@gmail.com>
2022-07-03 11:47:06 +0530

    Minor bugs fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,60 +16,58 @@ export default function Header() {
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     event.matches ? setIsDark(true) : setIsDark(false);
   });
-  
 
-if (isDark) {
-  return (<>
-    <Navbar className='navbar navbar-expand-lg navbar-dark py-4 px-4 cdin' expand="lg">
-      <Container >
-        <Nav >
-          <Avatar className="mx-2 d-none d-md-block" alt="Anubhab Patnaik" src={a1} />
-          <Navbar.Brand href="#">ANUBHAV PATNAIK</Navbar.Brand>        </Nav>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
+
+  if (isDark) {
+    return (<>
+      <Navbar className='navbar navbar-expand-lg navbar-dark py-4 px-4 cdin' expand="lg">
+        <Container >
+          <Nav >
+            <Avatar className="mx-2 d-none d-md-block" alt="Anubhab Patnaik" src={a1} />
+            <Navbar.Brand href="#">ANUBHAV PATNAIK</Navbar.Brand>        </Nav>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+            </Nav>
+            <Nav>
+              <Nav.Link href="#tech">Tech</Nav.Link>
+              <Nav.Link href="#books">Books</Nav.Link>
+              <Nav.Link href="#movies-music-tv">Movies & TV</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>)
+  }
+  else {
+    return (<>
+      <Navbar className='navbar navbar-expand-lg py-4 px-4 cdin' expand="lg">
+        <Container >
+          <Nav >
+            <Avatar className="mx-2 d-none d-md-block" alt="Anubhab Patnaik" src={a1} />
+            <Navbar.Brand href="#">ANUBHAV PATNAIK</Navbar.Brand>
           </Nav>
-          <Nav>
-            <Nav.Link href="#tech">Tech</Nav.Link>
-            <Nav.Link href="#books">Books</Nav.Link>
-            <Nav.Link href="#movies-music-tv">Movies & TV</Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </>)
-}
-else {
-  return (<>
-    <Navbar className='navbar navbar-expand-lg py-4 px-4 cdin' expand="lg">
-      <Container >
-        <Nav >
-          <Avatar className="mx-2 d-none d-md-block" alt="Anubhab Patnaik" src={a1} />
-          <Navbar.Brand href="#">ANUBHAV PATNAIK</Navbar.Brand>
-        </Nav>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-          </Nav>
-          <Nav>
-            <Nav.Link href="https://instagram.com/anubhavclicks">Instagram</Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/anubhabpatnaik0530/">LinkedIn</Nav.Link>
-            <Nav.Link eventKey={2} href="#footer">
-              HitMeUp
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </>)
-}
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+            </Nav>
+            <Nav>
+              <Nav.Link href="#tech">Tech</Nav.Link>
+              <Nav.Link href="#books">Books</Nav.Link>
+              <Nav.Link href="#movies-music-tv">Movies & TV</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>)
+  }
 }
